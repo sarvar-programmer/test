0d01473dd1d5c150740ae6a1bfdc22bca3716991
Sarvar <sarvarakromov22@gmail.com>
2025-07-17 17:59:51 +0500

    komentlar ochirildi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./components/home/Home";
-import Books from "./components/books/Books";
-import Rahbariat from "./components/rahbariat/Rahbariat";
-import AKM from "./components/akm/AKM";
-import News from "./components/news/News";
 import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
 import Contact from "./components/contact/Contact";
 export default function App() {
 
@@ -15,13 +10,9 @@ export default function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/news" element={<News />} /> */}
-        {/* <Route path="/books" element={<Books />} /> */}
-        {/* <Route path="/akm" element={<AKM />} /> */}
-        {/* <Route path="/rahbariat" element={<Rahbariat />} /> */}
       </Routes>
       <Contact/>
-      {/* <Footer/> */}
+      
      
     </>
 
